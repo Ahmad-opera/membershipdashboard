f976ec8628210eb4831a1796fba574cd27465216
Ahmad Yusuf <ahmadee.by@gmail.com>
2023-04-01 18:00:09 +0100

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  cssLoaderOptions: {
+    url: false
+  },
+  throwIfNamespace: false,
 }
 
 module.exports = nextConfig
